1b5f1a28ed286b556219e9c56f5eadf97e7349da
Suryansh Soni <szscode2727@gmail.com>
2020-12-19 03:14:44 -0600

    work

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,7 +4,8 @@ import Header from "../components/header.js";
 import "../styles/portfolio.css";
 import Navbar from "../components/Navbar.js";
 import ParticlesBg from "particles-bg";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card, ListGroup, Carousel } from "react-bootstrap";
+import { Autorenew } from "@material-ui/icons";
 export default function Portfolio() {
   return (
     <div>
@@ -22,7 +23,7 @@ export default function Portfolio() {
   </div>*/}
 
       <div className="port__aboutme" id="port__aboutme">
-        <Card>
+        <Card className="port__aboutme">
           <Card.Header>About me</Card.Header>
           <Card.Body>
             <blockquote className="blockquote mb-0">
@@ -35,16 +36,50 @@ export default function Portfolio() {
         </Card>
       </div>
 
+      <div className="port__cara" id="port__cara">
+        <Carousel>
+          <Carousel.Item interval={1000}>
+            <a href="https://github.com/suryanshsoni27" className="d-block">
+              <img
+                href="https://github.com/suryanshsoni27"
+                className="d-block m-auto w-50 h-auto"
+                src="https://news.efinancialcareers.com/binaries/content/gallery/efinancial-careers/articles/2019/04/github.jpg"
+                alt="First slide"
+              />
+            </a>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <a href="linkedin.com/in/suryansh-soni" className="d-block">
+              <img
+                href="www.linkedin.com/in/suryansh-soni"
+                className="d-block m-auto w-50 h-100"
+                src="https://www.kinesisinc.com/wp-content/uploads/2020/04/linkedin-101-hero@2x.png"
+                alt="Third slide"
+              />
+            </a>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+
       {/*<div className="port__signUp">
         <h1>sign up</h1>
       </div>
 */}
 
       <div className="port__contactme" id="port__contactme">
-        <Card style={{ width: "120rem" }}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>My Email : szscode2727@gmail.com</ListGroup.Item>
-            <ListGroup.Item>Phone number : +1(334)-332-4627</ListGroup.Item>
+        <Card className="port__contactme" style={{ width: "auto" }}>
+          <ListGroup className="port__contactme" variant="flush">
+            <ListGroup.Item className="port__contactme">
+              My Email : szscode2727@gmail.com
+            </ListGroup.Item>
+            <ListGroup.Item className="port__contactme">
+              Phone number : +1(334)-332-4627
+            </ListGroup.Item>
+            <ListGroup.Item className="port__contactme">
+              <a href="linkedin.com/in/suryansh-soni">
+                linkedin.com/in/suryansh-soni
+              </a>
+            </ListGroup.Item>
           </ListGroup>
         </Card>
       </div>
